186eb33c3de1404003b90a9cf0756eb87cb8298b
Wes <108998223+wesgunning@users.noreply.github.com>
2023-04-12 16:29:27 +0200

    Add script to HTML file & basic math functions to script file

---
+++ script.js
@@ -0,0 +1,26 @@
+let total = 0;
+// Basic Functions
+
+// Add
+function add(num) {
+    total += num;
+    return total;
+}
+
+// Subtract
+function subtract(num) {
+    total -= num;
+    return total;
+}
+
+// Multiply
+function multiply(num) {
+    total *= num;
+    return total;
+}
+
+// Divide
+function divide(num) {
+    total /= num;
+    return total;
+}
