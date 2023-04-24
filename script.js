32bf14442bcd41faafd62a61ccf01c9065ac5ca7
Wes <108998223+wesgunning@users.noreply.github.com>
2023-04-24 14:35:36 +0200

    Change add/subtract/multiply/divide functions. Add operate function

--- script.js
+++ script.js
@@ -6,30 +6,42 @@ let secondNumber;
 // Basic Functions
 
 // Add
-function add(num) {
-    total += num;
-    return total;
+function add(num1, num2) {
+    let sum = num1 + num2;
+    return sum;
 }
 
 // Subtract
-function subtract(num) {
-    total -= num;
-    return total;
+function subtract(num1, num2) {
+    let difference = num1 - num2;
+    return difference;
 }
 
 // Multiply
-function multiply(num) {
-    total *= num;
-    return total;
+function multiply(num1, num2) {
+    let product = num1 * num2;
+    return product;
 }
 
 // Divide
-function divide(num) {
-    total /= num;
-    return total;
+function divide(num1, num2) {
+    let dividend = num1 / num2;
+    return dividend;
 }
 
 // Operate
 function operate(operator, firstNumber, secondNumber) {
-    
+    if (operator == "+") {
+        add(firstNumber, secondNumber);
+    }
+    else if (operate == "-") {
+        subtract(firstNumber, secondNumber);
+    }
+    else if (operater == "*") {
+        multiply(firstNumber, secondNumber);
+    }
+    else if (operator == "/") {
+        divide(firstNumber, secondNumber);
+        
+    }
 }
